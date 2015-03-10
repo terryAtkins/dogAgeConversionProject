7a5ad0bec3a4dc8d6f2c1356658deb6e3c861c75
terryAtkins <git2hubs@gmail.com>
2015-03-10 10:04:09 +0000

    add dog age converter files

--- dog years/ViewController.swift
+++ dog years/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var dogYearsTextField: UITextField!
+    @IBOutlet weak var dogCalculatedYearsLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,15 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func dogCalculateButtonPressed(sender: UIButton) {
+        let converstionNumber = 7.0
+        dogCalculatedYearsLabel.hidden = false
+        let dogYearsInHumanYears = Double((dogYearsTextField.text as NSString).doubleValue)
+        dogCalculatedYearsLabel.text = "Your dog's is \(dogYearsInHumanYears * converstionNumber) years old"
+        dogYearsTextField.text = ""
+        dogYearsTextField.resignFirstResponder()
+        
     }
 
 
